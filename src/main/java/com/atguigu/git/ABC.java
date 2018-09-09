7f6afe8a3b6e82ad07b50ef5ff44618ac9ad7c66
wangxin <1003578071@qq.com>
2018-09-09 23:27:56 +0800

    hot_fix

--- src/main/java/com/atguigu/git/ABC.java
+++ src/main/java/com/atguigu/git/ABC.java
@@ -5,5 +5,7 @@ public class ABC {
 	public static void main(String[] args) {
 		System.out.println("TestGit......");
 		System.out.println("TestGit2......");
+		
+		System.out.println("TestGit_hot_fix......");
 	}
 }
