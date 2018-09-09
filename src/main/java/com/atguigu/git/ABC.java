3c1e9ec4a2bbf43e699172115c0be7af4a2062d6
wangxin <1003578071@qq.com>
2018-09-09 23:34:12 +0800

    edit hot_fix

--- src/main/java/com/atguigu/git/ABC.java
+++ src/main/java/com/atguigu/git/ABC.java
@@ -4,6 +4,9 @@ public class ABC {
 
 	public static void main(String[] args) {
 		
-		System.out.println("TestGit......");
+		
+		System.out.println("TestGit1......");
+		System.out.println("TestGit2......");
+		System.out.println("TestGit3......");
 	}
 }
